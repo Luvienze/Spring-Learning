4b411eb8bd9b774e5c40191bb2a360f1ef0b5058
Eren Sönmez <testuser@gmail.com>
2024-05-14 13:02:27 +0300

    Files are created for qualifiers

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/luvienze/springcoredemo/rest/DemoController.java
@@ -0,0 +1,29 @@
+package com.luvienze.springcoredemo.rest;
+
+import com.luvienze.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define private field for depeendency
+
+    private Coach myCoach;
+
+    // define constructor for dependency injection
+
+    @Autowired
+    public DemoController(Coach theCoach){
+
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}
+
+
