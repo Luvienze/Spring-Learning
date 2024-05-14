26abc8cf9e1e0f7ce85f582e55b379c3148ec92e
Eren Sönmez <testuser@gmail.com>
2024-05-14 11:37:49 +0300

    Files are created for Component Scanning

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luvienze/springcoredemo/Coach.java
@@ -0,0 +1,5 @@
+package com.luvienze.springcoredemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luvienze/springcoredemo/DemoController.java
@@ -0,0 +1,27 @@
+package com.luvienze.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define private field for depeendency
+
+    private Coach myCoach;
+
+    // define constructor for dependency injection
+
+    @Autowired
+    public DemoController(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}
+
+
