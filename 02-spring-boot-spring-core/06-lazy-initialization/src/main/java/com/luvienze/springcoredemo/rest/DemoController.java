2436ba2edd25bbbe29cb9e4716d5f053c0df9960
Eren Sönmez <testuser@gmail.com>
2024-05-14 13:53:12 +0300

    Added quailifer to use cricket coach

--- 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luvienze/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/luvienze/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.luvienze.springcoredemo.rest;
 
 import com.luvienze.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class DemoController {
     // define constructor for dependency injection
 
     @Autowired
-    public DemoController(Coach theCoach){
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
 
         myCoach = theCoach;
     }
