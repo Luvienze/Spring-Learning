d449e14cd0fdb441e3af7fcdf8710b1ff493b7a8
Eren Sönmez <testuser@gmail.com>
2024-05-14 11:54:36 +0300

    Used scanBasePackages in the springbootapp anotation to scan packages

--- 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luvienze/springcoredemo/SpringcoredemoApplication.java
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/luvienze/springcoredemo/SpringcoredemoApplication.java
@@ -3,7 +3,10 @@ package com.luvienze.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {"com.luvienze.springcoredemo",
+							"com.luvienze.util"}
+)
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
