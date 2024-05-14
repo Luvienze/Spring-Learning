98a1c12037b57ddd187604687fa6444cc199cfb4
Eren Sönmez <testuser@gmail.com>
2024-05-14 11:12:12 +0300

    Method is created and declared GetMapping as dailyworkout

--- 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luvienze/springcoredemo/DemoController.java
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luvienze/springcoredemo/DemoController.java
@@ -1,6 +1,7 @@
 package com.luvienze.springcoredemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,4 +16,12 @@ public class DemoController {
     public DemoController(Coach theCoach){
         myCoach = theCoach;
     }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
 }
+
+
