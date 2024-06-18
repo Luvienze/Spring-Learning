4ec5b81645529b0a3779458def8bbb7d45ae0e89
Eren Sönmez <testuser@gmail.com>
2024-06-18 12:15:26 +0300

    Removed Lazy Initialization from BeanScopes

---
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/luvienze/springcoredemo/rest/DemoController.java
@@ -0,0 +1,30 @@
+package com.luvienze.springcoredemo.rest;
+
+import com.luvienze.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define private field for depeendencyd
+
+    private Coach myCoach;
+
+    // define constructor for dependency injection
+
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}
+
+
