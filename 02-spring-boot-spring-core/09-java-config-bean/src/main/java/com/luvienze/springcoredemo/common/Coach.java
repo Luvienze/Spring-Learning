a085823984ea4dcb7a9362f0197d3d0f90abbc4f
Eren Sönmez <testuser@gmail.com>
2024-06-18 13:00:45 +0300

    Removed postConstruct and preDestroy methods

---
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luvienze/springcoredemo/common/Coach.java
@@ -0,0 +1,5 @@
+package com.luvienze.springcoredemo.common;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luvienze/springcoredemo/rest/DemoController.java
@@ -0,0 +1,32 @@
+package com.luvienze.springcoredemo.rest;
+
+import com.luvienze.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define private field for depeendencyd
+
+    private Coach myCoach;
+
+    // define constructor for dependency injection
+
+    //default scope is singleton
+    @Autowired
+    public DemoController
+            (@Qualifier("cricketCoach") Coach theCoach){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}
+
+
