875f550e8319bc498744f4d7780b275e1f45ccfc
Eren Sönmez <testuser@gmail.com>
2024-06-18 13:09:49 +0300

    Bean is created

--- 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luvienze/springcoredemo/config/SportConfig.java
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luvienze/springcoredemo/config/SportConfig.java
@@ -1,4 +1,15 @@
 package com.luvienze.springcoredemo.config;
 
+import com.luvienze.springcoredemo.common.Coach;
+import com.luvienze.springcoredemo.common.SwimCoach;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SportConfig {
+
+    @Bean
+    public Coach swimCoach(){
+        return new SwimCoach();
+    }
 }
