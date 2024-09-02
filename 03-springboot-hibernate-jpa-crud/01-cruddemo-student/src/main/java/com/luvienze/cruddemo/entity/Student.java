916c3f55636258205e479c1c22d4b429f6e631f8
Eren Sönmez <testuser@gmail.com>
2024-09-02 16:27:07 +0300

    Setting up for JPA is completed

---
+++ 03-springboot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luvienze/cruddemo/entity/Student.java
@@ -0,0 +1,85 @@
+package com.luvienze.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "student")
+public class Student {
+
+    // define fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private int id;
+
+    @Column (name = "first_name")
+    private String firstName;
+
+    @Column (name = "last_name")
+    private String lastName;
+
+    @Column (name = "email")
+    private String emai;
+
+
+    // define constructors
+
+    public Student (){
+
+    }
+
+    public Student(String firstName, String lastName, String emai) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emai = emai;
+    }
+
+    // define getters/setters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmai() {
+        return emai;
+    }
+
+    public void setEmai(String emai) {
+        this.emai = emai;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    // define toString() method
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", emai='" + emai + '\'' +
+                '}';
+    }
+}
