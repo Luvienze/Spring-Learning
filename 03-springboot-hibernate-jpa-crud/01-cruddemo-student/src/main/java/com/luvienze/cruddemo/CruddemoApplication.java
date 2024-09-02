0b87ff6266c6568d0d1697a6e22bc0fe73be6189
Eren Sönmez <testuser@gmail.com>
2024-09-02 16:49:15 +0300

    Created student dao and its implementation

--- 03-springboot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luvienze/cruddemo/CruddemoApplication.java
+++ 03-springboot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luvienze/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luvienze.cruddemo;
 
+import com.luvienze.cruddemo.dao.StudentDao;
+import com.luvienze.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,10 +16,24 @@ public class CruddemoApplication {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner commandLineRunner (String[] args)
+	public CommandLineRunner commandLineRunner (StudentDao studentDao)
 	{
 		return runner ->{
-			System.out.println("Hello World");
+			createStudent(studentDao);
 		};
+	}
+
+	private void createStudent(StudentDao studentDao) {
+
+		// create the student object
+		System.out.println("Creating a new student object...");
+		Student tempStudent = new Student("Paul","Doe","paul@gmail.com");
+
+		// save the student object
+		System.out.println("Saving the student ....");
+		studentDao.save(tempStudent);
+
+		// display the id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 }
---
+++ 03-springboot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luvienze/cruddemo/dao/StudentDao.java
@@ -0,0 +1,7 @@
+package com.luvienze.cruddemo.dao;
+
+import com.luvienze.cruddemo.entity.Student;
+
+public interface StudentDao {
+    void save(Student theStudent);
+}
