d57f82c1e9446aa2cc54f5f53b8d3db2680630dc
Eren Sönmez <testuser@gmail.com>
2024-09-03 15:01:11 +0300

    New method is implemented to the interface

--- 03-springboot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luvienze/cruddemo/dao/StudentDao.java
+++ 03-springboot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luvienze/cruddemo/dao/StudentDao.java
@@ -2,8 +2,12 @@ package com.luvienze.cruddemo.dao;
 
 import com.luvienze.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDao {
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
 }
