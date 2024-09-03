6f9a79e3c2f57e499ed90b05ce8d042b4c3a88e0
Eren Sönmez <testuser@gmail.com>
2024-09-03 15:03:21 +0300

    findAll method is completed

--- 03-springboot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luvienze/cruddemo/dao/StudentDAOImpl.java
+++ 03-springboot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luvienze/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.luvienze.cruddemo.dao;
 
 import com.luvienze.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDao{
@@ -29,5 +32,15 @@ public class StudentDAOImpl implements StudentDao{
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+        // return query results
+        return theQuery.getResultList();
     }
 }
