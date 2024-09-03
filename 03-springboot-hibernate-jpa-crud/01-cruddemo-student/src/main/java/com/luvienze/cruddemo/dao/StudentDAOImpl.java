fec999ff9fe68bc08a20eb3c59f0a5185789bbb1
Eren Sönmez <testuser@gmail.com>
2024-09-03 16:07:02 +0300

    Merge method is written

--- 03-springboot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luvienze/cruddemo/dao/StudentDAOImpl.java
+++ 03-springboot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luvienze/cruddemo/dao/StudentDAOImpl.java
@@ -57,4 +57,11 @@ public class StudentDAOImpl implements StudentDao{
         // return query results
         return theQuery.getResultList();
     }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+
+    }
 }
