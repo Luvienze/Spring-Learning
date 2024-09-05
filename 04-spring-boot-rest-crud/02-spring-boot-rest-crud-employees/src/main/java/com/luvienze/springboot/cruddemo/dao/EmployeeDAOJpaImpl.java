94808bfcb3173a3ec0729e30edb47574a17e301d
Eren Sönmez <testuser@gmail.com>
2024-09-05 21:39:07 +0300

    New methods are created

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employees/src/main/java/com/luvienze/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employees/src/main/java/com/luvienze/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -32,4 +32,26 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO{
 
         return employees;
     }
+
+    @Override
+    public Employee findById(int theID) {
+        Employee theEmployee = entityManager.find(Employee.class,theID);
+
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        Employee dbEmployee = entityManager.merge(theEmployee);
+
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theID) {
+        Employee theEmployee = entityManager.find(Employee.class,theID);
+
+        entityManager.remove(theEmployee);
+
+    }
 }
