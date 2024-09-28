71b3fc14147fb8c1ecd461019af5bf2280ae01b9
Eren Sönmez <testuser@gmail.com>
2024-09-28 10:50:04 +0300

    GetMapping and PostMapping

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luvienze/springboot/ThymeleafDemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luvienze/springboot/ThymeleafDemo/controller/HelloWorldController.java
@@ -4,14 +4,17 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
     // new a controller method to show initial HTML form
 
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
@@ -35,6 +38,21 @@ public class HelloWorldController {
 
         // create the message
         String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message",result);
+
+        return "helloworld";
+    }
+
+    @PostMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey My Friend from v3! " + theName;
 
         // add message to the model
         model.addAttribute("message",result);
