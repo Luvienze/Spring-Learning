adae50b2a720d2af3d6b1d1a7c6dc75c64b9d129
Eren Sönmez <testuser@gmail.com>
2024-10-05 17:12:53 +0300

    list is injected from app.prop file

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luvienze/springboot/ThymeleafDemo/controller/StudentController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luvienze/springboot/ThymeleafDemo/controller/StudentController.java
@@ -1,16 +1,21 @@
 package com.luvienze.springboot.ThymeleafDemo.controller;
 
 import com.luvienze.springboot.ThymeleafDemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
 
-    
+    // injtected list of countries from app.prop
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
