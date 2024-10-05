edae7426dbd84fbac5c00b5c25fdbd1f44b50338
Eren Sönmez <testuser@gmail.com>
2024-10-05 17:11:12 +0300

    list of countries added

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luvienze/springboot/ThymeleafDemo/controller/StudentController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luvienze/springboot/ThymeleafDemo/controller/StudentController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class StudentController {
 
+    
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel){
 
