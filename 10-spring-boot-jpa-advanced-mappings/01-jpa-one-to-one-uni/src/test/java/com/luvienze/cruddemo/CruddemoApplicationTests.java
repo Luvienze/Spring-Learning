351034d42abcf8510173a038b6c3e0aa7789d3a4
Eren Sönmez <testuser@gmail.com>
2024-11-15 18:07:09 +0300

    jpa one to one

---
+++ 10-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/test/java/com/luvienze/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luvienze.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
