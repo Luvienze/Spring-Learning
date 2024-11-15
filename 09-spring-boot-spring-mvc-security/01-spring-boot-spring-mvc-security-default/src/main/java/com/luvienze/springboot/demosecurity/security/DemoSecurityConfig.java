b3d1479d1d258f7b764470bd34876fe57d86da1e
Eren Sönmez <testuser@gmail.com>
2024-11-15 14:54:46 +0300

    db tables for authorization is added

--- 09-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luvienze/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 09-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luvienze/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,10 +3,22 @@ package com.luvienze.springboot.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
+
+    // add support for JDBC ... no more hardcoded users :-)
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource datasource){
+        return new JdbcUserDetailsManager(datasource);
+    }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
